770f0ddd1952886b96f1338f275ea1f9f0ed0b0f
Koungoins <56229062+Koungoins@users.noreply.github.com>
2019-10-07 12:51:59 +0300

    Update triselection.js

--- triselection.js
+++ triselection.js
@@ -1,9 +1,55 @@
 (function(){
 	
-  let liste = [7, 8, 6, 1, 3, 5]
+  let liste = [7, 2, 8, 6, 1, 3, 5, 4, 9]
   
   function tri_selection(l){
-  	/* à implementer */
+  	/* à implementer 
+    		en prenant comple le html pour animer la fonction
+        donc prevoir aussi des temps de pause à chaque "action"
+       	*/
+        
+        
   }
+  
+  function printCounter() {
+    console.log(counter);
+    counter++;
+    if (counter < 10)
+        setTimeout(printCounter, 1000);
+	}
+  
+  function tri_bulle(l){
+  	/* à implementer 
+    		en prenant comple le html pour animer la fonction
+        donc prevoir aussi des temps de pause à chaque "action"
+       	*/
+        	
+          /*
+          	HTML pure 
+           	*/
+          let curseur = document.getElementById("curseur");
+        	
+        
+        	let changed;
+          let counter = 0;
+   				do{
+        		changed = false;
+        		for(let i=0; i < l.length-1; i++) {
+            	
+            	if(l[i] > l[i+1]) {
+                let tmp = l[i];
+                l[i] = l[i+1];
+                l[i+1] = tmp;
+                changed = true;
+            	}
+              curseur.classList.add("simpleright");
+              let e = printCounter();
+              curseur.classList.remove("simpleright");
+        		}
+    			} while(changed);  
+  }
+  
+  
+  tri_bulle(liste);
   
 })();
