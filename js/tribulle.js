0a821b6d681f252e02c8b2677a7aca0dae679bd7
Ansuf B <bina.ansf@gmail.com>
2019-10-09 06:51:10 +0300

    Tri à bulles Ok
    Mise en évidence du curseur et affichage d'un message à la fin du tri
    Il faut juste revoir comment afficher toute la case lors du déplacement.

--- js/tribulle.js
+++ js/tribulle.js
@@ -21,32 +21,39 @@
     setTimeout(function(){
       console.log(liste);
       console.log(lis);
+      //HTML Suppression du style de selection
       for(let j=0; j<lis.length; j++){
         lis[j].classList.remove("animate-lft");
         lis[j].classList.remove("animate-rgt");
+		lis[j].classList.remove("current_position");  
       }
-      curseur.classList.remove("simpleright");
+      //curseur.classList.remove("simpleright");
 
+	  lis[i].classList.add("current_position"); 
 
       // on delay le chgt de nom
+      //Changement de position dans le HTML, changment du contenu
       if(namechange) {
-        lis[i-1].innerText = v1 
+        lis[i-1].innerText = v1
         lis[i].innerText = v2
         namechange = false;
       }
 
       if(i<liste.length-1){
-
+		//lis[i].classList.add("current_position");  
         if(liste[i] > liste[i+1]){
+          //Echange de position
           let tmp = liste[i];
           liste[i] = liste[i+1];
           liste[i+1] = tmp;
+          //Il y a eu un changement
           changed = true;
+          //On va inverser le contenu des items
           namechange = true;
           
-          //HTML
-          lis[i].classList.add("animate-lft");
-          lis[i+1].classList.add("animate-rgt");
+          //HTML mise en évidence des selections
+         lis[i].classList.add("animate-lft");
+         lis[i+1].classList.add("animate-rgt");
 
           //on delaye le chgt de nom à la prochaine iteration
           v1 = liste[i];
@@ -56,17 +63,21 @@
 
         }
         i++;
-        curseur.classList.add("simpleright");
+        //curseur.classList.add("simpleright");
         forloop();
       }
       else if(changed){
         i = 0;
         forloop();
         changed = false;
-      }
+      } else if(changed==false) {
+		  document.getElementById("fin").innerText="Terminé"
+	  }
+	  
     }, 2000)
   }
 
   forloop();
+  
 
 })();
