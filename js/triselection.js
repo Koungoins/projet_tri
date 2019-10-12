9fef92af6ea7efb0fb65877b02e722bf992eb4c6
Youssouf SAID <boomerkuigi@gmail.com>
2019-10-12 11:26:42 +0300

    not finished

--- js/triselection.js
+++ js/triselection.js
@@ -22,94 +22,46 @@
   */
 
   let i = 0;
+  let j = 0;
+
+  let ftimeout, stimeout;
+
   let changed = false;
   let namechange = false;
-  let v1, v2;
-  let iGrand=-1;
+
   let current;
 
-  function forloop() {
-    setTimeout(function(){
-      console.log(liste);
-      console.log(lis);
-      //HTML Suppression du style de selection
-      for(let j=0; j<lis.length; j++){
-        lis[j].classList.remove("animate-lft");
-        lis[j].classList.remove("animate-rgt");
-		lis[j].classList.remove("current_position");  
-		lis[j].classList.remove("position_grand");
-      }
-	  current = liste[i]
-      //curseur.classList.remove("simpleright");
 
-		  lis[i].classList.add("current_position"); 
-
-		//Recherche du plus grand que i depuis le début de la liste
-		console.log("Ici ="+liste[i]);
-		for(let a=0; a<i; a++) {				
-			if(liste[a]>liste[i]){
-				iGrand = a;		
-				lis[iGrand].classList.add("position_grand"); 			
-				console.log("Grand="+liste[a]);
-				break;
-			}
-		}
-		
-		if(iGrand>=0) {
-			//Décalage des autres 
-			for(d=i;d>=iGrand;d--) {
-				liste[d]=liste[d-1];
-				//lis[d].classList.add("animate-rgt");         
-				console.log("d-1 : "+(d-1));
-				if(d-1>=0){
-					lis[d].innerText = lis[d-1].innerText		
-				}			
-			}
-			liste[iGrand]=current;
-			lis[iGrand].innerText = current;
-			console.log("Apres decalage");
-			console.log(liste);
-			iGrand=-1;
-		}
-      if(i<liste.length-1){
-		//lis[i].classList.add("current_position");  
-        /*if(liste[i] > liste[i+1]){
-          //Echange de position
-          let tmp = liste[i];
-          liste[i] = liste[i+1];
-          liste[i+1] = tmp;
-          //Il y a eu un changement
-          changed = true;
-          //On va inverser le contenu des items
-          namechange = true;
+  function secondloop(){
+      return setTimeout(function(){
+        console.log("   j:"+j);
+        if(j <= 3 ){
           
-          //HTML mise en évidence des selections
-         lis[i].classList.add("animate-lft");
-         lis[i+1].classList.add("animate-rgt");
+          for(let k=0; k<lis.length; k++){
+            lis[k].classList.remove("simpleright");
+            lis[k].classList.add("simpleright"); 
+          }
+          
+          j++;
 
-          //on delaye le chgt de nom à la prochaine iteration
-          v1 = liste[i];
-          v2 = liste[i+1];
-          //lis[i].innerText = liste[i];
-          //lis[i+1].innerText = liste[i+1];
-
-        }*/
-        i++;
-        //curseur.classList.add("simpleright");
-        forloop();
-      }
-      else if(changed){
-        i = 0;
-        forloop();
-        changed = false;
-      } else if(changed==false) {
-		  document.getElementById("fin").innerText="Terminé"
-	  }
-	}, 2000)
-    
+          secondloop();
+        }
+        
+      }, 1000)
   }
 
-  forloop();
-  
+  function firstloop(){
+    return setTimeout(function(){
+      
+      clearTimeout(stimeout);
+      stimeout = secondloop();
+      
+      console.log("i:"+i);
+      i++;
+      j=0;
+      firstloop();
+    }, 6000);
+  }
 
+  firstloop();
 })();
