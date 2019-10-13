ba6b630f185d7294b5dcf70439f0bf3c412e658b
Youssouf SAID <boomerkuigi@gmail.com>
2019-10-13 10:22:08 +0300

    insertion tri ok (avec decoupage) . a faire: generer les anims dynamiquement et les integrer

--- js/triinsertion.js
+++ js/triinsertion.js
@@ -21,97 +21,53 @@
 
 	*/
 
-	let i = 0;
-	let changed = false;
-	let namechange = false;
-	let v1, v2;
-	let iMini=0;
+	let i = 1;
+	let j = 0;
+
+	let stimeout;
+	let iTemp = -1;
 	let current;
 
-	function forloop() {
-		setTimeout(function(){
-			console.log(liste);
-			console.log(lis);
-			//HTML Suppression du style de selection
-			for(let j=0; j<lis.length; j++){
-				lis[j].classList.remove("animate-lft");
-				lis[j].classList.remove("animate-rgt");
-				lis[j].classList.remove("current_position");  
-				lis[j].classList.remove("position_grand");
-			}
-			current = liste[i]
-			//curseur.classList.remove("simpleright");
-
-			lis[i].classList.add("current_position"); 
-
-			//Recherche du plus petit que i depuis le i+1 de la liste
-			console.log("Ici ="+liste[i]);
-			iMini = i;
-			for(let a=i; a<liste.length; a++) {						
-				if(liste[a]<liste[iMini]){
-					iMini = a;										
-				}
-			}
-		
-		
-			if(iMini>=0) {
-				lis[iMini].classList.add("position_grand"); 			
-				console.log("Petit="+liste[iMini]+" que ="+liste[i]);
-				console.log("Avant échange");
-				console.log(liste);
-				//Echange de place
-				let tmp = liste[i];
-				liste[i] = liste[iMini];
-				liste[iMini] = tmp;
-				
-				
-				lis[i].innerText = lis[iMini].innerText;
-				lis[iMini].innerText = tmp;
-				
-				
-				console.log("Apres échange");
-				console.log(liste);
-				iMini=-1;
-			}
-			if(i<liste.length-1){
-			//lis[i].classList.add("current_position");  
-				/*if(liste[i] > liste[i+1]){
-					//Echange de position
-					let tmp = liste[i];
-					liste[i] = liste[i+1];
-					liste[i+1] = tmp;
-					//Il y a eu un changement
-					changed = true;
-					//On va inverser le contenu des items
-					namechange = true;
-					
-					//HTML mise en évidence des selections
-				 lis[i].classList.add("animate-lft");
-				 lis[i+1].classList.add("animate-rgt");
-
-					//on delaye le chgt de nom à la prochaine iteration
-					v1 = liste[i];
-					v2 = liste[i+1];
-					//lis[i].innerText = liste[i];
-					//lis[i+1].innerText = liste[i+1];
-
-				}*/
+	function secondloop(){
+		return setTimeout(function(){
+			console.log("   j:"+j);
+			if(j >=0 && liste[j] > iTemp){
+				liste[j+1] = liste[j];
+				j--;
+				secondloop();
+			} else if( j === -1 || liste[j] <= iTemp) {
+				liste[j+1] = iTemp;
 				i++;
-				
-				forloop();
 			}
-			else if(changed){
-				i = 0;
-				forloop();
-				changed = false;
-			} else if(changed==false) {
-			document.getElementById("fin").innerText="Terminé"
-		}
-	}, 2000)
-		
+		}, 600)
 	}
+  
+	function firstloop(){
+	  return setTimeout(function(){
+		
+		if(i >= liste.length){
+			//terminé
+			console.log('terminé');
+			console.log(liste);
+			return;
+		}
 
-	forloop();
+		console.log(liste);
+		
+		iTemp = liste[i];
+		j = i-1;
+
+		console.log('i:', i, " iTemp:", iTemp);
+
+		clearTimeout(stimeout);
+		stimeout = secondloop();
+		
+		
+		firstloop();
+	  }, (liste.length+1)*600);
+	}
+  
+	firstloop();
 	
 
 })();
