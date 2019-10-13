39800e7993713556fab185e7efad9c0f5e06a12a
Youssouf SAID <boomerkuigi@gmail.com>
2019-10-13 14:06:46 +0300

    triinsertion ok

--- js/triinsertion.js
+++ js/triinsertion.js
@@ -6,14 +6,44 @@
 	let lis = document.getElementById("tablist").getElementsByTagName("li");
 	
 	//Récupération de la liste UL pour le traitement
-	 for (let i = 0;i<lis.length;i++){
+	for (let i = 0;i<lis.length;i++){
 		liste.push(parseInt(lis[i].innerText));
 	}
 	
 	console.log(liste);
 	console.log(lis);
  
- 
+	function dynamicAnimation(index) {
+		//creer une animation dynamique pour l'élement à caser;
+
+		let style = document.createElement('style');
+		style.type = 'text/css';
+		let keyFrames = '\
+		.move_place {\
+		  animation: anim_left 0.5s;\
+		  animation-fill-mode: forwards;\
+		}\
+		  \
+		@keyframes anim_left {\
+			0% {\
+				transform: translateX(0px) translateY(0px);\
+			}\
+			20% {\
+				transform: translateX(0px) translateY(-25px);\
+			}\
+			60% {\
+				transform: translateX(DYNC_VAL) translateY(-25px);\
+			}\
+			100% {\
+				transform: translateX(DYNC_VAL) translateY(0px);\
+			}\
+		}\
+		';
+		style.innerHTML = keyFrames.replace(/DYNC_VAL/g, "-"+(index*50)+"px");
+		document.getElementsByTagName('head')[0].appendChild(style);
+	
+		return "move_place";
+	  }
  
 	/*
 
@@ -23,10 +53,10 @@
 
 	let i = 1;
 	let j = 0;
-
+	let jPos = -1;
 	let stimeout;
 	let iTemp = -1;
-	let current;
+	let deplacement = false;
 
 	function secondloop(){
 		return setTimeout(function(){
@@ -35,9 +65,46 @@
 				liste[j+1] = liste[j];
 				j--;
 				secondloop();
-			} else if( j === -1 || liste[j] <= iTemp) {
-				liste[j+1] = iTemp;
+			}else if(deplacement){
+				//dernier tour de boucle. on clear les animations et on change le html
+				for(let m=0; m<lis.length; m++){
+					lis[m].classList.remove("simpleright");
+					lis[m].classList.remove("move_place");
+					lis[m].classList.remove("current_position");
+				}
+				console.log("debug: jPos:", jPos)
+				const jText = lis[i].innerText;
+				for(let n=i-1; n>jPos-1; n--){
+					lis[n+1].innerText = lis[n].innerText
+				}
+				lis[jPos].innerText = jText;
 				i++;
+				deplacement = false;
+				return;
+			} 
+			else if( j === -1 || liste[j] <= iTemp) {
+				console.log("ne doit s'afficher qu'une seul fois");
+				//tri
+				liste[j+1] = iTemp;
+				jPos = j+1;
+				const index = i - (j+1);
+
+				//l'élement est deja a la bonne position
+				if(index === 0){
+
+				}
+				else {
+					// on anime le déplacement 
+					const anim = dynamicAnimation( index );
+					lis[i].classList.add(anim);
+					for(let k=i-1; k>=j+1; k--){
+						lis[k].classList.add("simpleright");
+					}
+				}
+				//i++;
+				j--;
+				deplacement = true;
+				secondloop();
 			}
 		}, 600)
 	}
@@ -59,12 +126,14 @@
 
 		console.log('i:', i, " iTemp:", iTemp);
 
+		lis[i].classList.add("current_position")
+
 		clearTimeout(stimeout);
 		stimeout = secondloop();
 		
 		
 		firstloop();
-	  }, (liste.length+1)*600);
+	  }, (liste.length+2)*600);
 	}
   
 	firstloop();
