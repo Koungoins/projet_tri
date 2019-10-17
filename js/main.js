d33ec12496d03da63b6a6e45182da34a5cf830f8
Ansuf B <bina.ansf@gmail.com>
2019-10-17 06:05:39 +0300

    Choix du tri
    Affciahge du tri selectionné et masquer les autres.
    Voir comment démarré seulement à l'affichage.

--- js/main.js
+++ js/main.js
@@ -5,15 +5,19 @@
 		console.log(window.location.href)
 	}
 
+
 	choix = function(){
 		
 		let slct = document.getElementsByName('tri')[0];
 		let tri = slct.value;
-		//console.log(tri, window.location);
-		windowFileProtocolChange('pages/tri'+tri+'.html');
-
-
+		let contener = document.getElementsByTagName("iframe");
+		//Cacher tout les iframes avant de montrer celui choisie.
+		for(let i=0; i<contener.length; i++) {
+			contener[i].style.display = "none"
+		}
+		
+		let triChoise = document.getElementById(tri);
+		triChoise.style.display = "block";
 	}
-
 
 })();
